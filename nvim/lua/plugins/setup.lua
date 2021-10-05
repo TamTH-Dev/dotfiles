a76a39387cad95cbb49d1a8378be81d86a4595f4
TamTH-Dev <tamth.dev@gmail.com>
2021-10-05 18:24:56 +0700

    Update nvim

--- nvim/lua/plugins/setup.lua
+++ nvim/lua/plugins/setup.lua
@@ -60,10 +60,10 @@ end
 function M.close_tag()
   return function()
     local global = vim.g
-    -- File extensions where this plugin is enabled
-    global.closetag_filenames = '*.html,*.xhtml,*.phtml,*.js,*.ts,*.vue'
-    -- List of non-closing tags self-closing in the specified files
+    global.closetag_filenames = '*.html,*.xhtml,*.phtml,*.js,*.ts,*.vue,*.jsx,*.tsx,*.vuex'
     global.closetag_xhtml_filenames = '*.xhtml,*.jsx,*.tsx,*.vuex'
+    global.closetag_filetypes = 'html,xhtml,phtml,js,ts,vue,jsx,tsx,vuex'
+    global.closetag_xhtml_filetypes = 'xhtml,jsx,tsx,vuex'
     -- Make the list of non-closing tags case-sensitive (e.g. `<Link>` will be>
     global.closetag_emptyTags_caseSensitive = 1
   end

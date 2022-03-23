20cb351bc68b83d57d0ba3165c09bc2c9c69e38e
TamTH-Dev <tamth.dev@gmail.com>
2022-03-23 08:58:13 +0700

    Update nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -248,7 +248,7 @@ packer.startup(function()
     setup = setup.close_buffers(),
   }
 
-  -- use {
-  --   'tweekmonster/startuptime.vim',
-  -- }
+  use {
+    'tweekmonster/startuptime.vim',
+  }
 end)

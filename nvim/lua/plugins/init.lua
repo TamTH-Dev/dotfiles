9f3761d6d4ad84fc4b4e23b3e675d328470b879a
TamTH-Dev <tamth.dev@gmail.com>
2023-01-12 09:11:05 +0700

    Update nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -319,7 +319,7 @@ packer.startup(function()
 	})
 
 	--[[ Measure neovim's startup time ]]
-	use({
-		"tweekmonster/startuptime.vim",
-	})
+	-- use({
+  --		"tweekmonster/startuptime.vim",
+	-- })
 end)

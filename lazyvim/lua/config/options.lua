79df473a6222bc51771f43b4fd5dbd29d0f306b7
tamth3 <tamth3@vng.com.vn>
2026-07-27 10:38:09 +0700

    Update lazyvim configuration

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,15 +2,24 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
---[[ Leader key ]]
+-- Leader key
 vim.g.mapleader = ","
---[[ Save buffer ]]
+
+-- Enable Soft-Wrapping (Visual Display)
+vim.opt.wrap = true
+-- Avoid breaking words in the middle
+vim.opt.linebreak = true
+
+-- Save buffer
 vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>w!<CR>", { noremap = true, silent = true })
---[[ Quit buffer ]]
+
+-- Quit buffer
 vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>q<CR>", { noremap = true, silent = true })
---[[ Exit modes ]]
+
+-- Exit modes
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
---[[ Move between windows ]]
+
+-- Move between windows
 vim.api.nvim_set_keymap("n", "<A-k>", "<C-W>k", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-l>", "<C-W>l", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<A-j>", "<C-W>j", { noremap = true, silent = true })
